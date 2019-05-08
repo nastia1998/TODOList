cd97e5731ee1195c9abd5b02a29e93083d33d0ca
Nastya <leshencko.nastia2013@gmail.com>
2019-05-08 18:34:34 +0300

    added view model for user and todolist

---
+++ TODOList.WEB/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODOList.WEB.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Passwork { get; set; }
+    }
+}
