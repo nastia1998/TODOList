9f02305182c3bea102618608014b6b765e489c62
Nastya <leshencko.nastia2013@gmail.com>
2019-05-08 18:38:23 +0300

    updated task model and created view model for task class

--- TODOList.WEB/Models/TaskViewModel.cs
+++ TODOList.WEB/Models/TaskViewModel.cs
@@ -1,26 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace TODOList.DAL.Entities
+namespace TODOList.WEB.Models
 {
-    public class Task 
+    public class TaskViewModel
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public DateTime DateCompletion { get; set; }
         public DateTime DateReminder { get; set; }
-
         public int TodoListId { get; set; }
-
-        [ForeignKey("TodoListId")]
-        public TodoList TodoList { get; set; }
-
-        public ICollection<Step> Steps { get; set; }
     }
 }
