48adc2c64e531ed91bc2db3e1578f628ef09cb4d
Nastya <leshencko.nastia2013@gmail.com>
2019-05-08 18:40:55 +0300

    updated step model and create view model for step class

--- TODOList.WEB/Models/StepViewModel.cs
+++ TODOList.WEB/Models/StepViewModel.cs
@@ -1,24 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace TODOList.DAL.Entities
+namespace TODOList.WEB.Models
 {
-    public class Step
+    public class StepViewModel
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public bool IsDone { get; set; }
-
         public int TaskId { get; set; }
-
-        [ForeignKey("TaskId")]
-        public Task Task { get; set; }
-
     }
 }
