5757339f4062f861531f0a21d4f883f988b1e46a
Nastya <leshencko.nastia2013@gmail.com>
2019-05-10 14:39:18 +0300

    add controller for user

---
+++ TODOList.WEB/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TODOList.BLL.DTO;
+using TODOList.BLL.Services;
+
+namespace TODOList.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService userService;
+
+        public UsersController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserDTO> Get()
+        {
+            var users = userService.GetAll();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<UserDTO> Get(int id)
+        {
+            var user = await this.userService.Get(id);
+            user.Login = "";
+            user.Password = "";
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<UserDTO> SignUp([FromBody] UserDTO user)
+        {
+            var res = await userService.Create(user);
+            return res;
+        }
+
+        [HttpPut("{id}")]
+        public async void Put(int id, [FromBody] UserDTO user)
+        {
+            int res = await userService.Update(id, user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<UserDTO> Delete(int id)
+        {
+            return await userService.Delete(id);
+        }
+
+    }
+}
