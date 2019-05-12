f16c2924bfcb04913c6652b575c304e2ab55ca7d
Nastya <leshencko.nastia2013@gmail.com>
2019-05-12 22:42:41 +0300

    nothing change

--- TODOList.BLL/Interfaces/IService.cs
+++ TODOList.BLL/Interfaces/IService.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace TODOList.BLL.Interfaces
 {
-    interface IService
+    public interface IService<T>
     {
+        Task<T> Create(T obj);
+        Task<T> Get(int id);
+        IEnumerable<T> GetAll();
+        Task<int> Update(int id, T obj);
+        Task<T> Delete(int id);
     }
 }
