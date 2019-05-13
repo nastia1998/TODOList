ef8faf7d06e5209e7d7d363c6707ea9dcc6ef666
Nastya <leshencko.nastia2013@gmail.com>
2019-05-13 21:26:48 +0300

    add email field to model user

--- TODOList.DAL/Entities/User.cs
+++ TODOList.DAL/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace TODOList.DAL.Entities
         public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
         public ICollection<TodoList> TodoLists { get; set; }
     }
 }
